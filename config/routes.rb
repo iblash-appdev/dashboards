8401f8700768d000b54d0f706ec25102961ae8bb
Milton Lash <iblash@live.com>
2023-03-17 21:10:15 +0000

    Third currency

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get("/forex/:from_currency", { :controller => "currencies", :action => "second_currency"})
 
-  get("/forex/:from_currency/:from_currency", { :controller => "currencies", :action => "third_currency"})
+  get("/forex/:from_currency/:to_currency", { :controller => "currencies", :action => "third_currency"})
 
   
 
